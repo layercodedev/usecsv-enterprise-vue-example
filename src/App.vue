abe74676d36c578249081cc68017d1aff957f683
Hanan <28744817+HananAlsawy@users.noreply.github.com>
2024-02-07 09:02:24 +0200

    Add UseCSV

--- src/App.vue
+++ src/App.vue
@@ -1,47 +1,32 @@
-<script setup lang="ts">
-import HelloWorld from './components/HelloWorld.vue'
-import TheWelcome from './components/TheWelcome.vue'
+<script setup>
+window.usecsv = {
+  importerKey: 'e3033735-73be-498a-9d3c-0a8b4c070e67',
+  user: { userId: 123 },
+  metadata: { anotherId: 12345 },
+  batchSize: 1000,
+  onData: (data) => console.log({ data }),
+  onClose: () => console.log('importer is closed')
+  // ... other options
+}
 </script>
 
 <template>
-  <header>
-    <img alt="Vue logo" class="logo" src="./assets/logo.svg" width="125" height="125" />
-
-    <div class="wrapper">
-      <HelloWorld msg="You did it!" />
-    </div>
-  </header>
-
-  <main>
-    <TheWelcome />
-  </main>
+  <h1>UseCSV example in Vue.js</h1>
+  <button id="usecsv-importer-button">Import Data</button>
+  <div id="usecsv-importer-root"></div>
 </template>
 
 <style scoped>
-header {
-  line-height: 1.5;
-}
-
-.logo {
-  display: block;
-  margin: 0 auto 2rem;
-}
-
-@media (min-width: 1024px) {
-  header {
-    display: flex;
-    place-items: center;
-    padding-right: calc(var(--section-gap) / 2);
-  }
-
-  .logo {
-    margin: 0 2rem 0 0;
-  }
-
-  header .wrapper {
-    display: flex;
-    place-items: flex-start;
-    flex-wrap: wrap;
-  }
+#usecsv-importer-button {
+  width: 170px;
+  height: 70px;
+  color: #fff;
+  background-color: #027ad6;
+  box-shadow: 0 32px 64px rgba(0, 0, 0, 0.07);
+  font: 700 24px sans-serif;
+  text-align: center;
+  border: none;
+  border-radius: 3px;
+  cursor: pointer;
 }
 </style>
